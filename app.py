d5afc432fc2d94f3d68de6b7c1cddff551ea3685
Konsta Janhunen <kvjanhun@gmail.com>
2022-01-25 19:42:15 +0200

    Create environment, update .gitignore

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask import redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
