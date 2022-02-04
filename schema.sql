23deaf94c0844c2fd9e249bf9676b68fa31dd072
kvjanhun <kvjanhun@gmail.com>
2022-02-04 08:14:00 +0200

    Add SQL schema

---
+++ schema.sql
@@ -0,0 +1,32 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT,
+    password TEXT,
+    admin BOOLEAN
+);
+
+CREATE TABLE user_info (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    names TEXT,
+    phone TEXT
+);
+
+CREATE TABLE checkpoint (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    theme TEXT,
+    location TEXT
+);
+
+CREATE TABLE groups (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    size INTEGER
+);
+
+CREATE TABLE points (
+    id SERIAL PRIMARY KEY,
+    group_id INTEGER REFERENCES groups,
+    total INTEGER
+);
