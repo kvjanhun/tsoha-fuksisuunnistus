b526cc122c4855f5c2197f21620dcedbb3c52fe7
kvjanhun <kvjanhun@gmail.com>
2022-03-03 12:27:47 +0200

    Rename 'groups' as 'teams'

---
+++ teams.py
@@ -0,0 +1,15 @@
+from db import db
+
+def create_team(name):
+    try:
+        sql = "INSERT INTO groups (name) VALUES (:name)"
+        db.session.execute(sql, {"name":name})
+        db.session.commit()
+        return True
+    except:
+        return False
+
+def get_teams():
+    sql = "SELECT * FROM teams"
+    return db.session.execute(sql).fetchall()
+
